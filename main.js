9d1326b08ed8e0fc12c8e58dd561f98499a0c779
CC11001100 <CC11001100@qq.com>
2022-10-31 19:07:54 +0800

    refactor, add init attribute detector

--- main.js
+++ main.js
@@ -17,22 +17,30 @@ class WindowMonitor {
      * @returns {Promise<void>}
      */
     async startWindowMonitor() {
+
+        // 第一次的认为是原生携带的，不触发事件
+        await this.screenshotWindow(true);
+
+        // 之后的才触发事件
         while (true) {
-            await this.screenshotWindow();
-            await this.sleep(100);
+            await this.screenshotWindow(false);
+            await this.sleep(300);
         }
     }
 
-    async screenshotWindow() {
+    async screenshotWindow(isInit) {
         for (let key in window) {
+
             // 已经存在的情况
             if (this.windowAttributeSet.has(key)) {
                 continue;
             }
 
             // 触发新增事件
-            for (let callback of this.windowListenerQueue) {
-                callback(key)
+            if (!isInit) {
+                for (let callback of this.windowListenerQueue) {
+                    callback(key)
+                }
             }
 
             this.windowAttributeSet.add(key);
